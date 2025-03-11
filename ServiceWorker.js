8c934e431e88733193b290499c1b05ceaef166f4
Ekhalios <leandre.sentenac@epitech.eu>
2025-03-11 17:41:57 +0100

    buiuld top fix

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Portfolio_Dev-0.1.0";
+const contentToCache = [
+    "Build/portfolio_vercel.loader.js",
+    "Build/portfolio_vercel.framework.js.gz",
+    "Build/portfolio_vercel.data.gz",
+    "Build/portfolio_vercel.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
