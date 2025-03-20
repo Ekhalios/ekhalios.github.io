ca67ee71f827aa05dce6036275d510e60f643e25
Ekhalios <leandre.sentenac@epitech.eu>
2025-03-20 15:01:22 +0100

    pwa

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Ekhalios-Portfolio-0.1.0";
+const contentToCache = [
+    "Build/portfolio_vercel.loader.js",
+    "Build/portfolio_vercel.framework.js",
+    "Build/portfolio_vercel.data",
+    "Build/portfolio_vercel.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
